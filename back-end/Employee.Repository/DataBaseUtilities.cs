645bf54d69ba2040c62bdfd9bcda758e9845b32f
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-21 00:36:19 +0530

    create Database Utilities class ane define method openConnection

---
+++ back-end/Employee.Repository/DataBaseUtilities.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Employee.Repository
+{
+    public class DataBaseUtilities
+    {
+        private SqlConnection _connection;
+
+        private string connectionString = "Server=localhost\\SQLEXPRESS;Database=EmpTrackProDB;Trusted_Connection=True;";
+
+
+        public void openConnection()
+        {
+            if (connectionString == null)
+            {
+                _connection = new SqlConnection(connectionString);
+            }
+
+            if(_connection.State != ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+    }
+}
