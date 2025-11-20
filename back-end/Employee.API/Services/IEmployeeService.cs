f604031faefac242327b62a06f1271f20fee5136
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-20 23:57:08 +0530

    create interface for employee service with must need all methods

---
+++ back-end/Employee.API/Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using Employee.Models.DTOs;
+
+namespace Employee.API.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<EmployeeDTO>> GetAllAsync();
+        Task<EmployeeDTO> GetByIdAsync(int employeeNo);
+        Task AddAsync(EmployeeDTO employeeDto);
+        Task UpdateAsync(EmployeeDTO employeeDto);
+        Task DeleteAsync(int employeeNo);
+        Task<decimal> GetAverageSalaryAsync();
+    }
+}
