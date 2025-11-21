b8f8b43d2724581e115ec89c85260b883dfb9ee8
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-21 09:48:57 +0530

    create controller class and define method Add employee

---
+++ back-end/EmpTrackPro.API/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using EmpTrackPro.API.Services;
+using EmpTrackPro.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpTrackPro.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService service)
+        {
+            _employeeService = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(EmployeeDTO employee)
+        {
+            await _employeeService.AddAsync(employee);
+            return Ok(new { Message = "Employee Added" });
+        }
+    }
+}
