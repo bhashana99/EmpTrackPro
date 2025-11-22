5213bbdae7b6d409ea923abd76be4f5c9a57bf16
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-22 12:53:19 +0530

    employee form design

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeFormComponent } from "./components/employee-form/employee-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- front-end/src/app/components/employee-form/employee-form.component.ts
+++ front-end/src/app/components/employee-form/employee-form.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css',
 })
-export class EmployeeFormComponent {
+export class EmployeeFormComponent implements OnInit {
 
+  employeeForm !: FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      employeeNumber:['',Validators.required],
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      dob:['',Validators.required],
+      salary:['',Validators.required],
+    })
+  }
+
+  onAdd(){
+    console.log("add button click");
+
+  }
+
+  
 }
